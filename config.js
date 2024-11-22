f864aa30361471035d6d1bfdb0c42f1a571016e7
KING-USMAN <mansoorking264@gmail.com>
2024-11-22 13:29:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&yU9RWDTA#H8UWvBVSmLS1IvcBhkhaSdyeWLlBelGKP7GYhgMt66k",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&TYU1RZTa#erxY7R03CQqCH6iH9_uQpwJoD-Tz4bgTKUDR2HaU3iA",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
 MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
@@ -67,9 +67,9 @@ ANTI_DELETE: process.env.ANTI_DELETE || "true",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING : process.env.AUTO_TYPING || "false",
 AUTO_RECORDING : process.env.AUTO_RECORDING || "false",
-OWNER_NUMBER : process.env.OWNER_NUMBER || "94742274855",
+OWNER_NUMBER : process.env.OWNER_NUMBER || "923173269442",
 DELCHAT : process.env.DELCHAT || "pm",
-FOOT : process.env.FOOT || "🧚‍♀️𝗠𝗔𝗡𝗨-𝗠𝗗-𝗩❼💗",
+FOOT : process.env.FOOT || "🧚‍♀️USMAN-𝗠𝗗-𝗩❼💗",
 BOT_INFO: process.env.BOT_INFO || "Manu-MD;OFC;0;https://i.ibb.co/GRpbTZp/IMG-20240920-WA0020.jpg;https://whatsapp.com/channel/0029VaN1XMn2ZjCsu9eZQP3R",
 HANDLERS: process.env.HANDLERS || ".,",
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1500': process.env.MAX_SIZE,
